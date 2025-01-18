6d1a3475b0de9e388db2bc81927ee1097c1ea65e
abdul565656 <abdullahkashif12b3@gmail.com>
2025-01-18 19:39:35 +0500

    DOCUMENTATION FOLDER ADDED

--- CODE_BASE/sanity/schemaTypes/index.ts
+++ CODE_BASE/sanity/schemaTypes/index.ts
@@ -1,8 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity';
 import product from './product';
-import category from './category'; // Import the category schema
+import category from './category'; 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product, category], // Add category here
+  types: [product, category], 
 };
 
