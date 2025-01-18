efe5123176dd45f8bd1b215bedced7cbb7937273
abdul565656 <abdullahkashif12b3@gmail.com>
2025-01-18 20:18:09 +0500

    HOMEPAGE2 UPDATED

--- CODE_BASE/app/Homepage2/page.tsx
+++ CODE_BASE/app/Homepage2/page.tsx
@@ -6,8 +6,28 @@ import Card1 from '../components/Card1'
 import Image from 'next/image'
 import { FaRegCheckCircle } from "react-icons/fa";
 import Footer from '../components/Footer'
+import { client } from "@/sanity/lib/client";
+import { urlFor } from "@/sanity/lib/image";
 
-const page = () => {
+const page = async () => {
+  const getProducts = async () => {
+        const products = await client.fetch(`
+          *[_type=="product"]{
+            name,
+            price,
+            image,
+            slug
+          }
+        `,
+        {},
+        {cache : 'no-store'}
+      );
+        return products;
+      };
+  
+    const allProducts = await getProducts();
+    console.log(allProducts);
+  
   return (
     <div>
        <Navbar />
@@ -37,7 +57,48 @@ const page = () => {
   </div>
 </div>
 <Hero2 />
-<Card1 />
+                                {/* cards display */}
+
+                                <div className="overflow-x-hidden">
+      {/* Header Section */}
+      <div className="px-2 md:px-8 py-12 text-[#2A254B] mt-12">
+        <h1 className="text-2xl font-semibold">New Ceramics</h1>
+      </div>
+
+      {/* Product Items */}
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-8 px-4">
+        {allProducts.map((product: any) => (
+          <div key={product._id} className="w-full">
+            {/* Card Image */}
+            {product.image && (
+              <div className="relative w-full h-[400px] bg-gray-200">
+                <img
+                  src={urlFor(product.image).width(600).height(800).url()}
+                  alt={product.name || "Product Image"}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            )}
+            {/* Card Content */}
+            <div className="mt-4 text-[#2A254B]">
+              <Link href={`/products/${product.slug.current}`}>
+                <p className="py-2 text-sm font-medium">{product.name}</p>
+              </Link>
+              <p className="text-sm font-semibold">${product.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* View Collection Button */}
+      <div className="flex justify-center items-center mt-20">
+        <Link href="/SeeAllProducts">
+          <button className="bg-[#F9F9F9] py-3 px-5 rounded-[5px] text-[#2A254B]">
+            View collection
+          </button>
+        </Link>
+      </div>
+    </div>
 
  {/* Hero Section 2 */}
               <div className='flex justify-center items-center space-x-4 md:p-10 p-6 md:flex-row flex-col'>
